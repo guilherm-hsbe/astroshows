dbb2120bdae5528bacc798048551c76ffa3ef444
guilherm-hsbe <guilherm3hsb@gmail.com>
2022-11-01 20:13:42 -0300

    awdawd

--- ProjIngressos/app/Models/Local.php
+++ ProjIngressos/app/Models/Local.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Local extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'endereco',
+        'cidade',
+        'estado',
+    ];
 }
