2a9f72130643320f1b6509f5bb1b51582d085042
GabrielSilvaMEL <gabrielsilvaalves3000@outlook.com>
2022-11-05 11:34:29 -0300

    up

---
+++ ProjIngressos/resources/views/site/home.blade.php
@@ -0,0 +1,21 @@
+@extends('site.layout')
+
+@section('content')
+
+
+  <div class="container marketing">
+
+    <!-- Three columns of text below the carousel -->
+    <div class="row">
+      <div class='col' style='background-color:orange'>
+        <h1>ReVeículos</h1>
+        <p>Vendemos carros usados</p>
+        
+      </div>
+      
+
+    </div><!-- /.row -->
+
+  </div><!-- /.container -->
+
+@endsection
