010b045c6f9a92a60cde7bfdcb745ec1778ed9b1
GabrielSilvaMEL <99516302+GabrielSilvaMEL@users.noreply.github.com>
2022-11-08 13:43:15 -0300

    Update HomeController.php

--- ProjIngressos/app/Http/Controllers/HomeController.php
+++ ProjIngressos/app/Http/Controllers/HomeController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function index(){
-        return view('site.home');
+        return view ('site.home');
     }
     
 }
