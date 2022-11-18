b84e04222c9923398320280f57ac90bfb264dac4
Guilherme HSBE <guilherm3hsb@gmail.com>
2022-11-18 16:46:54 -0300

    *

--- ProjIngressos/resources/views/site/artista.blade.php
+++ ProjIngressos/resources/views/site/artista.blade.php
@@ -2,17 +2,23 @@
 
 @section('content')
 
-<section id="slider" style='height:100'>
-  <input type="radio" name="slider" id="s1">
-  <input img='' type="radio" name="slider" id="s2">
-  <input type="radio" name="slider" id="s3" checked>
-  <input type="radio" name="slider" id="s4">
-  <input type="radio" name="slider" id="s5">
-  <label for="s1" id="slide1"><img class=" h-100" src="https://dynaimage.cdn.cnn.com/cnn/digital-images/org/cc4f8999-309e-494f-90b3-3af14f73ee0b.jpg" alt="First slide"></label>
-  <label for="s2" id="slide2"></label>
-  <label for="s3" id="slide3"></label>
-  <label for="s4" id="slide4"></label>
-  <label for="s5" id="slide5"></label>
-</section>
+<div style="padding-top:2rem; height:50%; background-color:black">
+  <section id="slider" class="card-fit">
+    <input type="radio" name="slider" id="s1" checked>
+    <input type="radio" name="slider" id="s2">
+    <input type="radio" name="slider" id="s3">
+    
+
+    <label for="s1" id="slide1">
+      <img class="h-100" src="{{ asset('images/artistas/scott-min.jpg') }}" alt="First slide">
+      <div class="text-img text-light">
+        <h4>Travis Scott</h4>
+        <p>Cantor de Rap</p>
+      </div>
+    </label>
+    <label for="s2" id="slide2"></label>
+    <label for="s3" id="slide3"></label>
+  </section>
+</div>
 
 @endsection  
--- ProjIngressos/resources/views/site/local.blade.php
+++ ProjIngressos/resources/views/site/local.blade.php
@@ -2,4 +2,33 @@
 
 @section('content')
 
+<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+        <div class="jumbotron jumbotron-fluid" style="height:94.2vh">
+            <div class="container">
+                <h1 class="display-4">Fluid jumbotron</h1>
+                <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+    
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+  </div>
+ <button class="carousel-control-prev" type="button" data-target="#carouselExampleControls" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-target="#carouselExampleControls" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </button>
+</div>
+
 @endsection
