ef940b23f22bec150f1d557aeb3ef7db2c397c37
GabrielSilvaMEL <gabrielsilvaalves3000@outlook.com>
2022-11-21 22:13:10 -0300

    Update artista.blade.php

--- ProjIngressos/resources/views/site/artista.blade.php
+++ ProjIngressos/resources/views/site/artista.blade.php
@@ -16,8 +16,24 @@
         <p>Cantor de Rap</p>
       </div>
     </label>
-    <label for="s2" id="slide2"></label>
-    <label for="s3" id="slide3"></label>
+
+
+    <label for="s2" id="slide2">
+    <img class="h-100" src="{{ asset('images/artistas/lamar.jpg') }}" alt="Second slide">
+      <div class="text-img text-light">
+        <h4>Kendrick Lamar</h4>
+        <p>Cantor de Rap</p>
+      </div>
+    </label>
+
+    
+    <label for="s3" id="slide3">
+    <img class="h-100" src="{{ asset('images/artistas/skrillex.jpg') }}" alt="Third slide">
+      <div class="text-img text-light">
+        <h4>Skrillex</h4>
+        <p>DJ</p>
+      </div>
+    </label>
   </section>
 </div>
 
