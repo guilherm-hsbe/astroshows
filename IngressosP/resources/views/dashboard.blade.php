7055c9007990d02902cb8d37f9bdc55d35837c57
Guilherme HSBE <guilherm3hsb@gmail.com>
2022-12-04 20:11:33 -0300

    *

--- IngressosP/resources/views/dashboard.blade.php
+++ IngressosP/resources/views/dashboard.blade.php
@@ -1,26 +1,32 @@
 <x-app-layout>
 @extends('site.layout')
 @section('content')
-    <div class="d-flex justify-content-center align-items-center mt-3 list-group">
-        <div class="mb-3">
-            <a class="nav-link btn btn-dark" href="{{ route('site.home') }}">Home</a>
-        </div>
+    
+    <?php 
+        $user = auth()->user();
+        if($user->name === "admin"){
+    ?>
+        <div class="d-flex justify-content-center align-items-center mt-3 list-group">
+            <div class="mb-3">
+                <a class="nav-link btn btn-dark" href="{{ route('site.home') }}">Home</a>
+            </div>
 
-        <div class="mb-2">
-            <a class="nav-link btn btn-outline-dark" href="{{ route('contatosmanager.index') }}">Contatos Manager</a>
-        </div>
+            <div class="mb-2">
+                <a class="nav-link btn btn-outline-dark" href="{{ route('contatosmanager.index') }}">Contatos Manager</a>
+            </div>
 
-        <div class="mb-2">
-            <a class="nav-link btn btn-outline-dark" href="{{ route('artistasmanager.index') }}">Artistas Manager</a>
-        </div> 
-        
-        <div class="mb-2">
-            <a class="nav-link btn btn-outline-dark" href="{{ route('locaismanager.index') }}">Locais Manager</a>
-        </div>  
-        
-        <div class="mb-2">
-            <a class="nav-link btn btn-outline-dark" href="{{ route('showsmanager.index') }}">Shows Manager</a>
+            <div class="mb-2">
+                <a class="nav-link btn btn-outline-dark" href="{{ route('artistasmanager.index') }}">Artistas Manager</a>
+            </div> 
+            
+            <div class="mb-2">
+                <a class="nav-link btn btn-outline-dark" href="{{ route('locaismanager.index') }}">Locais Manager</a>
+            </div>  
+            
+            <div class="mb-2">
+                <a class="nav-link btn btn-outline-dark" href="{{ route('showsmanager.index') }}">Shows Manager</a>
+            </div>
         </div>
-    </div>
+    <?php } ?>
 </x-app-layout>
 @endsection
