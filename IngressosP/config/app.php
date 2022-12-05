8fab4f230a973a247a59dec115f94160fb12f459
Guilherme HSBE <guilherm3hsb@gmail.com>
2022-12-04 22:06:02 -0300

    *dompdf app.php

--- IngressosP/config/app.php
+++ IngressosP/config/app.php
@@ -194,6 +194,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Barryvdh\DomPDF\ServiceProvider::class,
 
     ],
 
@@ -211,5 +212,7 @@ return [
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
     ])->toArray(),
+
+    'PDF' => Barryvdh\DomPDF\Facade::class,
 
 ];
